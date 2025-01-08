5b972d6f81537c2a9ba2c1b23cd7396b2e49fbf5
sithuuu888 <pakayagujjj@gmail.com>
2025-01-08 16:35:24 +0530

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+const fs = require('fs'):
+if (fs.existsSync('config.env')) require('dotenv').config((path: './config.env'}):
+
+funcrion convertetoBool(text, fault = true : false;
+ retern text === fault ? true : false;
+}
+module.exsports = {
+SESSION_ID: procces.envsession_id.
+}.
